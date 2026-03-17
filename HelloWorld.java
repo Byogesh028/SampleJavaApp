7b721be891be3298e53e9754ee81308797b830ea
Yogesh Bhavsar <109203365+Byogesh028@users.noreply.github.com>
2026-03-17 23:52:50 +0530

    changes added from server

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Welcome to Java programming.");
-        System.out.println("Welcome to Java programming From Git.");
-        
-        System.out.println("Welcome to Java programming From Git Again.");
-
-        System.out.println("Welcome to Java programming From VS Code.");
-
+        System.out.println("changes added from server");
+       
     }
 }
